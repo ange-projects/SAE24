8c210e16f69e206bf548bef6806c3ec229eaeb4b
corentin-iut <133323834+corentin-iut@users.noreply.github.com>
2023-06-15 11:22:53 +0200

    Update calcul_distance&cartographie_amplitude.py

--- scripts/calcul_distance&cartographie_amplitude.py
+++ scripts/calcul_distance&cartographie_amplitude.py
@@ -1,4 +1,5 @@
 import math
+import struct
 import scipy.constants
 
 room_size = (8, 8)  #Permit to modify the size of the room further in project
@@ -64,7 +65,7 @@ distance_case = distance(positions)
 
 #-----------------------------------
 
-amplitude = {}
+amplitude  = {}
 micro = ["micro1","micro2","micro3"]
 coef_permittivite = scipy.constants.epsilon_0
 
@@ -77,13 +78,44 @@ def dico_amplitude():
         amplitude_micro["am_micro"+str(id_micro+1)] = 0
       else:
         resultat = (coef_permittivite/(distance **2))
-        amplitude_micro["am_micro"+str(id_micro+1)] = resultat
+        resultat_arrondi = round(resultat,15)
+        amplitude_micro["am_micro"+str(id_micro+1)] = resultat_arrondi
     amplitude[id_case] = amplitude_micro
   return amplitude
 
 dico_amplitude = dico_amplitude()
+#print(dico_amplitude)
+
+
+def amplitude_a_binaire(nombre):
+
+  nbr_binaire = []
+  
+  packed = struct.pack('!d', nombre) 
+  
+  #converts the floating-point number into 8 bytes (64 bits) using the network byte order (big-endian)
+  #b'=F\x18\x85h\xe7\xbf\x97' This line gives a byte string of this type containing seven bytes, which
+  #are represented as ASCII characters or hexadecimal values.
+  
+  representation_binaire = [] #Initialise an empty list to store the binary representations of each byte
+
+  for octet in packed: #Browse every byte in packed
+  
+    valeur_binaire = bin(octet) #Convert the byte into binary, which returns a string of characters starting with '0b'.
+
+    valeur_binaire = valeur_binaire.replace('0b', '') #Remove the '0b' from the beginning of the string
+    
+    valeur_binaire = valeur_binaire.rjust(8, '0') #Ensure that the binary representation is of length 8 (since a byte contains 8 bits)
+
+    representation_binaire.append(valeur_binaire) #Add this binary representation to the list
+
+    nbr_binaire = ''.join(representation_binaire) #Concatenate all binary representations into a single string
+    
+  return nbr_binaire
+
+
+nombre_bin = amplitude_a_binaire(1.5e-13)
+print(nombre_bin)
 
 
 
-
-        
