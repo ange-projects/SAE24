6d6f89bfedd5c5231371fcaed7e9462335998d7e
pichaveroux <pierre.chaveroux.iut@gmail.com>
2023-06-18 16:40:11 +0200

    Script d'Injection terminé et focntionnel

--- scripts/injection_pos_cases.py
+++ scripts/injection_pos_cases.py
@@ -3,7 +3,7 @@ import calcul_distance_cartographie_amplitude
 
 # Connexion to database
 connexion = mysql.connector.connect(
-    host='192.168.103.189',
+    host='192.168.1.78',
     port='3306',
     database='bd_micros',
     user='brulix',
@@ -14,9 +14,9 @@ connexion = mysql.connector.connect(
 cursor = connexion.cursor()
 
 # Exécution d'une requête SQL pour insérer des données
-for position in calcul_distance_cartographie_amplitude.positions:
-    x, y = position
-    cursor.execute("INSERT INTO coord_cases (x, y) VALUES (%s, %s)", (x, y))
+for tabrecup in calcul_distance_cartographie_amplitude.tableau_de_pierre:
+    num_case, x, y, amp1, amp2, amp3 = tabrecup
+    cursor.execute("INSERT INTO coord_cases (num_case, x, y, MIC1, MIC2, MIC3) VALUES (%s, %s, %s, %s, %s, %s)", (num_case, x, y, amp1, amp2, amp3))
 
 # Valider la transaction
 connexion.commit()
