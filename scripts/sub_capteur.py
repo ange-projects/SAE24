ea3dd0e13ffaac3c413ca8b428769a8e01baa583
corentin-iut <133323834+corentin-iut@users.noreply.github.com>
2023-06-18 16:58:31 +0200

    Update sub_capteur.py

---
+++ scripts/sub_capteur.py
@@ -0,0 +1,59 @@
+import time
+import paho.mqtt.client as mqtt
+import calcul_distance_cartographie_amplitude.py
+
+#------------------------Mosquitto sub-------------------------
+
+#---Broker information---
+
+broker = "localhost"  
+port = 1883  
+topic = "SAE24/capteur" 
+messages = []
+
+#------------------------
+
+def connection(client, rc): #rc for return code
+    if rc == 0:
+        print("Connexion réussie")
+        client.subscribe(topic)  #subscription
+    else:
+        print(f"Erreur de connexion, code = {rc}")
+
+def message(client, msg):
+    print(f"Topic: {msg.topic}, Message: {msg.payload.decode()}")
+    messages.append(msg.payload.decode())
+
+def deconnection(client):
+    print("Déconnexion du broker")
+
+#----Callbacks----
+#A function which is passed as an argument to another function, and which is called at some point in the future
+
+client = mqtt.Client()  #client creation
+client.on_connect = connection 
+client.on_message = message  
+client.on_disconnect = deconnection  
+
+#---Attempt to connect to the broker---
+
+try: 
+    client.connect(broker, port) 
+except:
+    print("Erreur de connexion au broker")
+
+#---Background MQTT loop---
+
+client.loop_start() 
+
+try:
+    while True:
+        time.sleep(1) 
+except KeyboardInterrupt: 
+    pass 
+
+client.loop_stop() #Stopping the MQTT loop 
+client.disconnect() #Disconnecting the broker
+
+for message in messages:
+    print(message)
