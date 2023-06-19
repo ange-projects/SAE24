6fa689b5207a864efc223b2c04b6d22308739097
corentin-iut <133323834+corentin-iut@users.noreply.github.com>
2023-06-19 11:41:25 +0200

    Revert "Update injection_pos_cases.py"
    This reverts commit d64ae2d24062d5a245383178caebf8bdda3374ea.
    
    annulation du dernier commit

--- scripts/injection_pos_cases.py
+++ scripts/injection_pos_cases.py
@@ -1,73 +1,25 @@
-import time
-import paho.mqtt.client as mqtt
+import mysql.connector
 import calcul_distance_cartographie_amplitude
 
-#------------------------Mosquitto sub-------------------------
+# Connexion to database
+connexion = mysql.connector.connect(
+    host='192.168.1.78',
+    port='3306',
+    database='bd_micros',
+    user='brulix',
+    password='brul1goat'
+)
 
-#---Broker information---
+# Création d'un curseur pour exécuter des requêtes SQL
+cursor = connexion.cursor()
 
-broker = "localhost"
-port = 1883
-topic = "SAE24/capteur"
-messages = []
-stop = True
+# Exécution d'une requête SQL pour insérer des données
+for tabrecup in calcul_distance_cartographie_amplitude.tableau_de_pierre:
+    num_case, x, y, amp1, amp2, amp3 = tabrecup
+    cursor.execute("INSERT INTO coord_cases (num_case, x, y, MIC1, MIC2, MIC3) VALUES (%s, %s, %s, %s, %s, %s)", (num_case, x, y, amp1, amp2, amp3))
 
-#------------------------
+# Valider la transaction
+connexion.commit()
 
-
-def connexion(client, userdata, flags, rc):  #rc for return code
-  if rc == 0:
-    print("Connexion réussie")
-    client.subscribe(topic)  #subscription
-  else:
-    print(f"Erreur de connexion, code = {rc}")
-
-
-def message(client, userdata, msg):
-  global stop
-  print(f"Topic: {msg.topic}, Message: {msg.payload.decode()}")
-  messages.append(msg.payload.decode())
-  stop = False
-
-
-def deconnexion(client, userdata, rc):
-  print("Déconnexion du broker")
-
-
-#----Callbacks----
-#A function which is passed as an argument to another function, and which is called at some point in the future
-
-client = mqtt.Client()  #client creation
-client.on_connect = connexion
-client.on_message = message
-client.on_disconnect = deconnexion
-
-#---Attempt to connect to the broker---
-
-try:
-  client.connect(broker, port)
-except:
-  print("Erreur de connexion au broker")
-
-#---Background MQTT loop---
-
-client.loop_start()
-
-while stop:
-  time.sleep(1)
-
-client.loop_stop()  #Stopping the MQTT loop
-client.disconnect()  #Disconnecting the broker
-
-#--Information processing---
-
-resultat = []
-
-for message in messages:
-  print(message)
-  for element in message:
-    valeur = trouver_x_y(element)
-    resultat.append(valeur)
-
-print(resultat)
-
+# Fermeture de la connexion
+connexion.close()
