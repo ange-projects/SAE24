833c845ed6a499127d70376a7ee79a699dc17a2d
corentin-iut <133323834+corentin-iut@users.noreply.github.com>
2023-06-19 18:00:31 +0200

    Update degradation.py
    pour yoann

--- scripts/degradation.py
+++ scripts/degradation.py
@@ -1,4 +1,8 @@
 import random
+import calcul_distance_cartographie_amplitude
+
+#-----------------------Bit degradation----------------------
+
 
 def degradation_bit(valeur):
   id_micro, data = valeur[0:2], valeur[2:]
@@ -11,6 +15,14 @@ def degradation_bit(valeur):
   
   data_modif = ''.join(liste_bit)
   return id_micro + data_modif
-  
-  
+    
 #print(degradation_bit('010011110101000101100010000110011111101011111100001000010011100110'))
+
+def degradation(tableau):
+  for element in tableau:
+    valeur_errone = degradation_bit(element)
+    id_micro,data_errone = valeur_errone[0:2],valeur_errone[2:]
+    amplitude_errone = calcul_distance_cartographie_amplitude.binaire_a_amplitude(data_errone)
+    print(amplitude_errone)
+    
+print(degradation(['010011110101000101100010000110011111101011111100001000010011100110','010011110101000101100010000110011111101011111100001000010011100110','010011110101000101100010000110011111101011111100001000010011100110']))
