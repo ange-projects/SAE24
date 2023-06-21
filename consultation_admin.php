22cfeacf417be5559ebc35f00f58b1e224fa9826
Ange Giuntini <ange.giuntini.iut@gmail.com>
2023-06-21 10:19:56 +0200

    Correction de bug + menus admin
    correction du bug du plan historique pouvant être lancé plusieurs fois en simultané. Ajout de style pour les menus de sélection de l'admin

--- consultation_admin.php
+++ consultation_admin.php
@@ -20,51 +20,45 @@
     
     <section id="zero">
         <article id="left">
-            <span>Réseaux et Télécommunications | IUT de Blagnac</span>
+            <!-- <span>Réseaux et Télécommunications | IUT de Blagnac</span> -->
+            <h1>Page d'administration</h1>
             <h2>Bonjour <?php echo $_SESSION["login"]; ?> </h2> <!--Welcome banner-->
             <p>En tant qu'administrateur, vous avez accès à des fonctionnalités avancées pour surveiller et analyser les données en temps réel.</p>
         </article>
     </section>
 
-    <h1>Dynamic Plan</h1>
 
-    <form action="degradation.php" method="POST"> 
-        <p> En utilisant les champs de sélection suivants, indiquez vos paramètres d'estimation. </p>
-        <p> Nombre de bits erronés lors de l'envoi : 
-            <select name="bits_errones">
-                <option value="0">0 bits</option>
-                <option value="1">1 bit</option>
-                <option value="2">2 bits</option>
-            </select>
-        </p>
-        <p> Nombre de micros en fonction lors de l'envoi : 
-            <select name="micros_en_marche">
-                <option value="3">3 micros</option>
-                <option value="2">2 micros</option>
-                <option value="1">1 micro</option>
-            </select>
-        </p>
-        <p> Indiquez la vitesse de déplacement de l'onde sonore : 
+    <div class="consultation">
+        <svg id="plan" width="500" height="500"></svg>
+        <div class="center">
+            <form action="degradation.php" method="POST"> 
+                <h3> Indiquez vos paramètres d'estimation. </h3>
+                <br>
+                <p> Nombre de bits erronés : 
+                    <select name="bits_errones">
+                        <option value="0">0 bits</option>
+                        <option value="1">1 bit</option>
+                        <option value="2">2 bits</option>
+                    </select>
+                </p>
+                <p> Nombre de micros en fonction  : 
+                    <select name="micros_en_marche">
+                        <option value="3">3 micros</option>
+                        <option value="2">2 micros</option>
+                        <option value="1">1 micro</option>
+                    </select>
+                </p>
+                <p> Vitesse de déplacement de l'objet:
             <select name="micros_en_marche">
                 <option value="3"> Rapide </option>
                 <option value="2"> Moyenne </option>
                 <option value="1"> Faible </option>
             </select>
-        </p>
-
-        <input type="submit" value="Soumettre">
-    </form>
-
-    <script>
-        function updateValue(newValue) {
-            document.getElementById("intervalValue").textContent = newValue;
-        }
-    </script>
-
-
-
-    <div class="consultation">
-        <svg id="plan" width="500" height="500"></svg>
+                </p>
+                <br>
+                <button type="submit" class="submit-button">Submit</button>
+            </form>
+    </div>
     </div>
 
     <div class="consultation">
@@ -74,10 +68,17 @@
             <span id="intervalValue">0</span>
             <form id="display_history">
             <input type="range" name="interval" id="interval" min="-500" max="0" value="0" oninput="updateValue(this.value)">
+            <br>
             <button type="submit" class="submit-button">Submit</button>
-        </form>
+             </form>
         </div>
     </div>
+
+    <script>
+        function updateValue(newValue) {
+            document.getElementById("intervalValue").textContent = newValue + " minutes";
+        }
+    </script>
 
     <script src="https://d3js.org/d3.v6.min.js"></script>
     <script src="./scripts/plan.js"></script>
