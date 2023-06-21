98f52d5ea7d0cada0760b05381056327dec0eedc
corentin-iut <133323834+corentin-iut@users.noreply.github.com>
2023-06-21 14:20:58 +0200

    Update degradation.py
    ajout

--- scripts/degradation.py
+++ scripts/degradation.py
@@ -2,7 +2,6 @@ import random
 from math import sqrt
 
 
-
 #-----------------------Bit degradation----------------------
 
 
@@ -12,13 +11,13 @@ def degradation_bit(valeur,bit, pourcentage):
   data_origine = data
   data_origine = int(data, 2) #convert the original data into integers to calculate the difference later 
   pourcentage /= 100
-  maximum = pourcentage * data_origine #5% of original value
+  maximum = pourcentage * data_origine #1% of original value
 
   #the binary that is altered needs to be in range (1, 52) to exclude the mantisa and the sign number. However, to have a small mistake generated, we only want to modify the number by a magnitude that is high enough to generate some parasite. That is why we start the range later on.
 
   if bit == 2:
     while True:
-      index1, index2 = random.sample(range(14, 28), 2) 
+      index1, index2 = random.sample(range(14, 26), 2) 
       liste_bit[index1] = '1' if liste_bit[index1] == '0' else '0' #simulation of degradation
       liste_bit[index2] = '1' if liste_bit[index2] == '0' else '0' #ternary condition
       data_modif = ''.join(liste_bit) #for example '101010110011' as a string
@@ -31,7 +30,7 @@ def degradation_bit(valeur,bit, pourcentage):
         liste_bit[index2] = '1' if liste_bit[index2] == '0' else '0'
   if bit == 1:
     while True: 
-      index1 = random.choice(range(14,28)) 
+      index1 = random.choice(range(14,26)) 
       liste_bit[index1] = '1' if liste_bit[index1] == '0' else '0' 
       data_modif = ''.join(liste_bit)
       data_modif_int = int(data_modif, 2) 
@@ -46,12 +45,12 @@ def degradation_bit(valeur,bit, pourcentage):
     
 #print(degradation_bit('111111111111111111111111111111111111111111111111111111111111111111',2,2))
 
-def degradation(tableau, choix, pourcentage):
+def degradation(tableau_errone):
+  liste_case_min, liste_case_min2 = [], []
   dico_amplitude_errone, dico_distance = {}, {}
   
-  for index, element in enumerate(tableau):
-    valeur_errone = degradation_bit(element, choix,pourcentage)
-    id_micro,data_errone = valeur_errone[0:2], valeur_errone[2:]
+  for index, element in enumerate(tableau_errone):
+    id_micro,data_errone = element[0:2], element[2:]
     
     amplitude_errone = binaire_a_amplitude(data_errone)
     valeur_distance = sqrt(coef_permittivite/amplitude_errone)
@@ -59,21 +58,29 @@ def degradation(tableau, choix, pourcentage):
     dico_amplitude_errone['am_errone_mic' + str(index+1)] = amplitude_errone
     dico_distance["distance_mic" + str(index+1)] = valeur_distance
 
-    ecart_min, case_min = 10000, 0
+    ecart_min, case_min, ecart_min_2, case_min_2 = 10000, 0, 10000, 0
     
     for case, distances in distance_case.items():
       a_comparer = distance_case[case]['micro'+str(index+1)]
       distance_mic = dico_distance['distance_mic'+str(index+1)]
       ecart = abs(a_comparer - distance_mic)
-      if ecart < ecart_min:
+      if ecart <= ecart_min:
         ecart_min = ecart
         case_min = case
-    print(ecart_min, case)
+        if ecart < ecart_min_2:
+          ecart_min_2 = ecart
+          case_min_2 = case
+    liste_case_min.append(case_min)
+    liste_case_min2.append(case_min_2)
+    
+  print(liste_case_min)
+  print(liste_case_min2)
+
       
     
 
   print(dico_amplitude_errone)
   print(dico_distance)
 
-print(degradation(['010011110101010100100111100011100000000000110111101100010110000111', '100011110110000011011110010111110000011011000010001001011001010000', '110011110101000100101100000011101110110000011111011010110011011101'],1,0.01))
+print(degradation(['010011110100111011000001011000011101101110010110110000000100100000', '100011110101000101010000000101100100101101011101001110011110001101', '110011110110010001010011100110101010000111101110000101111001000011'],1,5))
 
